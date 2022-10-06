61748e0e443b750e65e12736a512e9b344b3ee69
Patrycja Buczkowska <buczkowska.p@gmail.com>
2022-10-06 17:13:09 +0200

    Add base input

--- components/filters/Filters.vue
+++ components/filters/Filters.vue
@@ -2,26 +2,22 @@
   <div class="filters">
     <div class="filters__search">
       <div class="search">
-        <input v-model="searchKeywords" @change="searchKeywordsChangeHandler" />
+        <BaseInput
+          v-model="searchKeywords"
+          placeholder="search for sth"
+          @change="searchKeywordsChangeHandler"
+        />
       </div>
     </div>
 
     <div class="filters__price">
       <div>
         <label>min price input</label>
-        <input
-          v-model="searchPrice.min"
-          class="min"
-          @change="searchPriceHandler"
-        />
+        <input v-model="searchPrice.min" class="min" @change="searchPriceHandler" />
       </div>
       <div>
         <label>max price input</label>
-        <input
-          v-model="searchPrice.max"
-          class="max"
-          @change="searchPriceHandler"
-        />
+        <input v-model="searchPrice.max" class="max" @change="searchPriceHandler" />
       </div>
     </div>
 
@@ -38,29 +34,31 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
+import BaseInput from "@/components/ui/atoms/BaseInput.vue";
 export default Vue.extend({
-  name: 'Filters',
+  name: "Filters",
+  components: { BaseInput },
   data() {
     return {
       searchKeywords: null,
       searchPrice: { min: 0, max: 10000 },
       searchRating: 0,
       options: [1, 2, 3, 4, 5],
-    }
+    };
   },
   methods: {
     searchKeywordsChangeHandler() {
-      this.$emit('filtered-keywords', this.searchKeywords)
+      this.$emit("filtered-keywords", this.searchKeywords);
     },
     searchPriceHandler() {
-      this.$emit('filtered-price', this.searchPrice)
+      this.$emit("filtered-price", this.searchPrice);
     },
     searchRatingHandler() {
-      this.$emit('filtered-rating', this.searchRating)
+      this.$emit("filtered-rating", this.searchRating);
     },
   },
-})
+});
 </script>
 
 <style lang="scss">
---
+++ components/ui/atoms/BaseInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <label>{{ label }}</label>
+    <input :value="value" v-bind="$attrs" v-on="getListeners" />
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+export default Vue.extend({
+  name: "BaseInput",
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    value: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {
+    getListeners() {
+      return {
+        ...this.$listeners,
+        change: ({ target: { value } }) => this.$emit("input", value),
+        // input: (event) => this.$emit('input', event.target.value)
+      };
+    },
+  },
+});
+</script>
